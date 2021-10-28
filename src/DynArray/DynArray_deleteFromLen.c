47d025997d22fab284395fc8724071e5becb9684
BugbearR <bugbearr@bugbearr.jp>
2021-10-28 01:29:06 +0000

    Add ofw_DynArray_deleteFromLen

--- src/DynArray/DynArray_deleteFromLen.c
+++ src/DynArray/DynArray_deleteFromLen.c
@@ -1,11 +1,14 @@
 #include <ofw/DynArray.h>
 
+#include <ofw/error.h>
+
 ofw_Result_t ofw_DynArray_deleteFromLen(ofw_DynArray_t *pThis, int32_t from, int32_t length, ofw_Error_t *pOutError)
 {
     ofw_Result_t myResult = ofw_Result_FAILED;
     ofw_Error_t myError = ofw_Error_UNKNOWN;
     if (!pThis)
     {
+        myError = ofw_Error_INVALID_OBJECT | ofw_Error_PARAM1;
         goto EXIT_FUNC;
     }
 
@@ -21,9 +24,14 @@ ofw_Result_t ofw_DynArray_deleteFromLen(ofw_DynArray_t *pThis, int32_t from, int
         goto EXIT_FUNC;
     }
 
-    pThis->elementSize = elementSize;
-    pThis->length = length;
-    pThis->pBuffer = pBuffer;
+    if (length > 0)
+    {
+        void *pSrc = ofw_DynArray_getPtrM(pThis, from + length);
+        void *pDst = ofw_DynArray_getPtrM(pThis, from);
+        size_t charLen = pThis->elementSize * length;
+        memmove(pDst, pSrc, charLen);
+        pThis->length -= length;
+    }
 
     myError = ofw_Error_NONE;
     myResult = ofw_Result_SUCCEEDED;
